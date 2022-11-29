94268b802173f5ffc02cbc6e47fa3e0eb7c4fdc3
Jana <jana.samson@hotmail.fr>
2022-11-29 12:42:00 -0500

    contact form validation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,24 @@
+import { useState } from 'react'
 import '../styles/ContactForm.scss'
 
 function ContactForm() {
+    const [formData, setFormData] = useState({
+        "name": "",
+        "email": "",
+        "tel": "",
+        "message": ""
+    })
+
+    function handleFormData(e) {
+        const input = e.target.name
+        const value = e.target.value
+
+        setFormData(previousFormData => ({
+            ...previousFormData,
+            [input]: value
+        }))
+    }
+
     return (
         <div className="contact-form">
             <div className="left">
@@ -13,18 +31,25 @@ function ContactForm() {
                         type="text" 
                         name="name" 
                         placeholder="Name" 
+                        value={formData.name}
+                        onChange={(e) => handleFormData(e)}
                         required
                     />
                     <input 
                         type="email" 
                         name="email" 
                         placeholder="Email address"
+                        value={formData.email}
+                        onChange={(e) => handleFormData(e)}
                         required
                     />
                     <input 
                         type="tel"
                         name="tel"
+                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                         placeholder="Phone" 
+                        value={formData.tel}
+                        onChange={(e) => handleFormData(e)}
                         required
                     />
                     <textarea 
@@ -32,7 +57,8 @@ function ContactForm() {
                         cols="30" 
                         rows="6"
                         placeholder="Your Message"
-                        required
+                        value={formData.message}
+                        onChange={(e) => handleFormData(e)}
                     />
                 </form>
                 <div className="submit-btn">
